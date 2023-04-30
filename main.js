e6d21123dab7bbb9845b2a20efddcc564c9bd16c
Rafael Queiroz <rafakroz@gmail.com>
2023-04-30 11:24:44 -0300

    Código evoluído. Teclas funcionando.

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla'); /* Acessando todas as teclas */
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;  /* Vairável */
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    // console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    // console.log(contador);
+
+}
